cc218ba4a9f075505778e8f080ceb2f30ea206d1
Nikolay Zhurba <gloomikon@gmail.com>
2020-03-27 22:12:46 +0200

    Course2Week3Task2 part1

---
+++ Course2Week3Task2/Course2Week3Task2/Course2Week3Task2/Code/TableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  TableViewController.swift
+//  Course2Week3Task2
+//
+//  Copyright © 2018 e-Legion. All rights reserved.
+//
+
+import UIKit
+
+class TableViewController: UIViewController {
+
+    // MARK: - IBOulets
+
+    @IBOutlet weak var tableView: UITableView! {
+        didSet {
+            tableView.dataSource = self
+            tableView.delegate = self
+        }
+    }
+
+    // MARK: - Private properties
+
+    private var photos: [Photo] = []
+    private var photoProvider: PhotoProvider?
+    private let cellIdentifier = "photoCell"
+
+    // MARK: - Life cycle
+
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        tableView.frame = view.frame
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        generatePhotos()
+
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellIdentifier)
+    }
+
+    // MARK: - Private functions
+
+    private func generatePhotos() {
+        photoProvider = PhotoProvider()
+        photos = (photoProvider?.photos())!
+    }
+
+}
+
+// MARK: - UITableViewDelegate
+
+extension TableViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Row selected")
+    }
+
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+      print("Accessory selected")
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension TableViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellIdentifier, for: indexPath)
+
+        cell.imageView?.image = photos[indexPath.row].image
+        cell.textLabel?.text = photos[indexPath.row].name
+        cell.accessoryType = .detailButton
+
+        return cell
+    }
+}
